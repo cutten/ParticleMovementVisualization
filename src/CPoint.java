89641e9913878f35ddc14b5368d6f951ecc2ba11
cutten <cutten54@gmail.com>
2018-05-26 21:07:01 +0300

    Base structure init

---
+++ src/CPoint.java
@@ -0,0 +1,16 @@
+import java.awt.*;
+import java.awt.geom.Ellipse2D;
+import java.awt.geom.Rectangle2D;
+import javax.swing.*;
+
+public class CPoint extends Ellipse2D.Double {
+
+    public boolean isConnected(CPoint p){
+        return false;
+    }
+
+    public void draw(){}
+
+
+}
+
---
+++ src/PathLine.java
@@ -0,0 +1,7 @@
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import javax.swing.*;
+
+public class PathLine  extends Rectangle2D.Double{
+
+}
